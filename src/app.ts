cddbb2ded9ef6c4132db285bde416c41c00103e5
Jakub Kubacki <kubacki.jk@gmail.com>
2022-01-15 23:33:38 +0100

    Parameters

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,9 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/health", (req: Request, res: Response) => res.sendStatus(200));
-app.get("/ab*cd", (req: Request, res: Response) => res.send("/ab*cd"));
-app.get("/abc/", (req: Request, res: Response) => res.send("/abc"));
+app.get("/api/books/:bookId/:authorId", (req: Request, res: Response) => {
+  return res.send(req.params);
+});
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
