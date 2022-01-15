b6bf5a40554cb0ddef04a437dc8bf3681c856936
Jakub Kubacki <kubacki.jk@gmail.com>
2022-01-15 23:29:35 +0100

    Matching

--- src/app.ts
+++ src/app.ts
@@ -4,39 +4,9 @@ const app = express();
 
 app.use(express.json());
 
-app
-  .route("/api/books")
-  .get((req: Request, res: Response) => {
-    return res.send("GET request");
-  })
-  .post((req: Request, res: Response) => {
-    return res.send("POST request");
-  })
-  .put((req: Request, res: Response) => {
-    return res.send("PUT request");
-  })
-  .all((req: Request, res: Response) => {
-    return res.send("X request");
-  });
-
-app.get("/", (req: Request, res: Response) => {
-  // return res.send("Hello World");
-  // return res.json({
-  //   foo: "bar",
-  //   hello: "world",
-  // });
-  return res.redirect("http://example.com");
-});
-
-app.post("/api/data", (req: Request, res: Response) => {
-  // eslint-disable-next-line no-console
-  console.log(req.body);
-  return res.sendStatus(200);
-});
-
-app.all("/api/all", (req: Request, res: Response) => {
-  return res.sendStatus(200);
-});
+app.get("/health", (req: Request, res: Response) => res.sendStatus(200));
+app.get("/ab*cd", (req: Request, res: Response) => res.send("/ab*cd"));
+app.get("/abc/", (req: Request, res: Response) => res.send("/abc"));
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
