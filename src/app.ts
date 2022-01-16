f1818b35e995932c64858a32d1b492ff5dfec326
Jakub Kubacki <kubacki.jk@gmail.com>
2022-01-16 18:27:46 +0100

    Cleanup for the new app

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,10 @@
-/* eslint-disable no-console */
-import express, { NextFunction, Request, Response } from "express";
-import helmet from "helmet";
-import routes from "./routes";
+import express from "express";
+import config from "config";
 
+const port = config.get<number>("port");
 const app = express();
 
-app.use(helmet());
-app.use(express.json());
-
-const middleware =
-  ({ name }: { name: string }) =>
-  (req: Request, res: Response, next: NextFunction) => {
-    res.locals.name = name;
-    next();
-  };
-
-app.use(middleware({ name: "Kuba" }));
-
-routes(app);
-
-app.listen(3000, () => {
+app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log("Application listening at http://localhost:3000");
+  console.log(`Application listening at http://localhost:${port}`);
 });
