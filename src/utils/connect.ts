317740b21aace1fc80cf49ef3c288d7fb1ef0efe
Jakub Kubacki <kubacki.jk@gmail.com>
2022-01-16 18:46:09 +0100

    Update connect.ts

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -9,7 +9,6 @@ async function connect() {
     await mongoose.connect(dbUri);
     logger.info("Connected to mongodb");
   } catch (error: unknown) {
-    // eslint-disable-next-line no-console
     logger.error(`Not connected to mongodb ${(error as Error).message}`);
 
     process.exit(1);
